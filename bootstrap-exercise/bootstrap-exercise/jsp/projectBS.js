f62714c92e8bc4422110157464c4bc91a4d5faf7
kellymatthew78 <mkelly@techtonicgroup.com>
2018-03-23 14:02:26 -0600

    Created the 1st section of the main content of homepage (reservation section)

---
+++ bootstrap-exercise/bootstrap-exercise/jsp/projectBS.js
@@ -0,0 +1,11 @@
+﻿//JS Global Varibles
+
+
+//JS Varibles
+
+//JS Functions
+$("input").datepicker();
+var datepicker1 = $("#datepicker-1").on("change", function () {
+    $("#datepicker-2").datepicker("option",
+        "minDate", datepicker1.val());
+});
