4b5965bf8dd70a88278bf338daa3df397f74685a
kellymatthew78 <mkelly@techtonicgroup.com>
2018-05-15 13:31:17 -0600

    updated code for dealing with image base64 encoding.

--- CBookLibrary/CBookLibrary/js/myJquery.js
+++ CBookLibrary/CBookLibrary/js/myJquery.js
@@ -21,13 +21,43 @@ function covertB64() {
 
 }
 
+function encodeImageFileAsURL(element) {
+    var file = element.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+        $("#hfb64").val(reader.result);
+        console.log('RESULT', reader.result)
+    }
+    reader.readAsDataURL(file);
+}
+
+
+function readImage(inputElement) {
+    var deferred = $.Deferred();
+
+    var files = inputElement.get(0).files;
+
+    if (files && files[0]) {
+        var fr = new FileReader();
+        fr.onload = function (e) {
+            deferred.resolve(e.target.result);
+        };
+        fr.readAsDataURL(files[0]);
+    } else {
+        deferred.resolve(undefined);
+    }
+
+    return deferred.promise();
+}
+
 $(document).ready(function () {
 
     //test button
     $("#btntest").on("click", function () {
-        let x = covertB64();
-        document.getElementById("demo").innerHTML = x;
-        imgTest = x;
+      
+        document.getElementById("demo").innerHTML = $("#hfb64").val();
+
+       
     });
 });
 
